607138d93eccf84f58773dd5e0c4ea256e630023
Mikhail Gorbov <mgorbov@maprtech.com>
2016-11-25 18:17:49 +0200

    implemented orders, order page improved items page

--- core/config.js
+++ core/config.js
@@ -1,36 +1,43 @@
 function config($stateProvider, $urlRouterProvider) {
-    $urlRouterProvider.otherwise("/login");
+  $urlRouterProvider.otherwise("/login");
 
-    $stateProvider
-        .state('login', {
-            url: "/login",
-            templateUrl: "core/login.html",
-            data: { pageTitle: 'Вход' }
-        })
-        .state('index', {
-            abstract: true,
-            url: "/index",
-            templateUrl: "core/content.html"
-        })
-        .state('index.items', {
-            url: "/items",
-            templateUrl: "core/items/items.html",
-            data: { pageTitle: 'Номенклатура' }
-        })
-        .state('index.orders', {
-            url: "/orders",
-            templateUrl: "core/orders/orders.html",
-            data: { pageTitle: 'Заказы' }
-        })
-        .state('index.order', {
-            url: "/order",
-            templateUrl: "core/order/order.html",
-            data: { pageTitle: 'Заказ' }
-        })
+  $stateProvider
+    .state('login', {
+      url: "/login",
+      templateUrl: "core/login.html",
+      data: {pageTitle: 'Вход'}
+    })
+    .state('index', {
+      abstract: true,
+      url: "/index",
+      templateUrl: "core/content.html"
+    })
+    .state('index.items', {
+      url: "/items",
+      templateUrl: "core/items/items.html",
+      data: {pageTitle: 'Номенклатура'}
+    })
+    .state('index.orders', {
+      url: "/orders",
+      templateUrl: "core/orders/orders.html",
+      data: {pageTitle: 'Заказы'}
+    })
+    .state('index.order', {
+      url: "/order",
+      templateUrl: "core/order/order.html",
+      data: {pageTitle: 'Заказ'},
+      params: {
+        number: null,
+        date: null,
+        author: null,
+        counterparty: null,
+        sum: null
+      }
+    })
 }
 angular
-    .module('ordering')
-    .config(config)
-    .run(function($rootScope, $state) {
-        $rootScope.$state = $state;
-    });
+  .module('ordering')
+  .config(config)
+  .run(function ($rootScope, $state) {
+    $rootScope.$state = $state;
+  });
--- core/items/controller.js
+++ core/items/controller.js
@@ -1,151 +1,183 @@
-angular.module('ordering').controller('ItemsCtrl', ['$scope', '$timeout', '$http', 'gridUtil',
-    function ($scope, $timeout, $http, gridUtil) {
+angular.module('ordering').controller('ItemsCtrl', ['$scope', '$timeout', '$http',
+  function ($scope, $timeout, $http) {
 
-        var allItems = [];
+    var allItems = [];
 
-        prepareItemGrid();
-        prepareCategoryGrid();
-        prepareCartGrid();
+    prepareItemGrid();
+    prepareCategoryGrid();
+    prepareCartGrid();
 
-        function prepareItemGrid() {
+    function prepareItemGrid() {
 
-            $scope.itemsGridScopeProvider = {
-                gridRowClick: function (rowItem) {
-                    var index = $scope.cartGridOptions.data.indexOf(rowItem.entity);
-                    if (index == -1){
-                        $scope.cartGridOptions.data.unshift(rowItem.entity);
-                        index = 0;
-                    }
+      $scope.itemsGridScopeProvider = {
+        gridRowClick: function (rowItem) {
+          var index = $scope.cartGridOptions.data.indexOf(rowItem.entity);
+          var entity;
 
-                    $scope.focusOnAmount(index);
-                }
-            };
+          if (index >= 0) {
+            entity = $scope.cartGridOptions.data[index];
+            $scope.cartGridOptions.data.splice(index, 1);
+          }else{
+            entity = rowItem.entity;
+          }
 
-            $scope.itemsGridOptions = {
-                enableColumnMenus: false,
-                enableFiltering: true,
-                flatEntityAccess: true,
-                fastWatch: true,
-                enableRowSelection: true,
-                multiSelect: false,
-                enableSelectAll: false,
-                enableRowHeaderSelection: false,
-                rowHeight: 25,
-                noUnselect: true,
-                columnDefs: [
-                    {name: 'Код', field: 'code', width: '10%'},
-                    {name: 'Артикул', field: 'article', width: '10%'},
-                    {name: 'Наименование', field: 'name', width: '60%'},
-                    {name: 'Цена', field: 'price', width: '10%', enableFiltering: false},
-                    {name: 'Остаток', field: 'stock', width: '10%', enableFiltering: false}
-                ],
-                onRegisterApi: function (gridApi) {
-                    $scope.itemsGridApi = gridApi;
-                },
-                appScopeProvider: $scope.itemsGridScopeProvider,
-                rowTemplate: '<div ng-dblclick="grid.appScope.gridRowClick(row)" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid" ' +
-                'class="ui-grid-cell" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }"  ui-grid-cell></div>',
+          index = $scope.cartGridOptions.data.length;
+          $scope.cartGridOptions.data.push(entity);
 
-                data: []
-            };
-
-            $http.get("data/items.json").then(function (response) {
-                $scope.itemsGridOptions.data = response.data;
-                allItems = response.data
-            });
+          $scope.focusOnAmount(index);
         }
+      };
 
-        function getChildrenCategories(row, ids) {
-            var children = row.treeNode.children;
-            if (children.length) {
-                children.forEach(function (child) {
-                    getChildrenCategories(child.row, ids)
-                });
-            } else {
-                ids.push(row.entity.id);
+      $scope.itemsGridOptions = {
+        enableColumnMenus: false,
+        enableFiltering: true,
+        // flatEntityAccess: true,
+        fastWatch: true,
+        enableRowSelection: true,
+        multiSelect: false,
+        enableSelectAll: false,
+        enableRowHeaderSelection: false,
+        rowHeight: 25,
+        noUnselect: true,
+        columnDefs: [
+          {name: 'Код', field: 'code', width: '10%'},
+          {name: 'Артикул', field: 'article', width: '10%'},
+          {name: 'Наименование', field: 'name', width: '60%'},
+          {name: 'Цена', field: 'price', width: '10%', enableFiltering: false},
+          {name: 'Остаток', field: 'stock', width: '10%', enableFiltering: false}
+        ],
+        onRegisterApi: function (gridApi) {
+          $scope.itemsGridApi = gridApi;
+          gridApi.cellNav.on.navigate($scope, function (newRowCol, oldRowCol) {
+            gridApi.selection.selectRow(newRowCol.row.entity);
+          });
+          gridApi.cellNav.on.viewPortKeyDown($scope, function (event, newRowCol) {
+            var row = newRowCol.row;
+            if (event.keyCode === 13) {
+              $scope.itemsGridScopeProvider.gridRowClick(row);
             }
-        }
+          });
+        },
+        keyDownOverrides: [{keyCode: 39, ctrlKey: false}],
+        appScopeProvider: $scope.itemsGridScopeProvider,
+        rowTemplate: '<div ng-dblclick="grid.appScope.gridRowClick(row)" ng-repeat="(colRenderIndex, col) in colContainer.renderedColumns track by col.uid" ' +
+        'class="ui-grid-cell" ng-class="{ \'ui-grid-row-header-cell\': col.isRowHeader }"  ui-grid-cell></div>',
 
-        function filterItemsByCategory(CategoriesRow) {
-            if (CategoriesRow.isSelected) {
-                var ids = [];
-                getChildrenCategories(CategoriesRow, ids);
-                $scope.itemsGridOptions.data = allItems.filter(function (item) {
-                    return ids.indexOf(item.categoryId) >= 0
-                });
-            } else {
-                $scope.itemsGridOptions.data = allItems
-            }
-        }
+        data: []
+      };
 
-        function prepareCategoryGrid() {
-            $scope.categoryGridOptions = {
-                enableColumnMenus: false,
-                enableSorting: true,
-                enableFiltering: true,
-                enableColumnMenu: false,
-                showTreeExpandNoChildren: false,
-                enableRowSelection: true,
-                enableRowHeaderSelection: false,
-                multiSelect: false,
-                rowHeight: 25,
-                columnDefs: [
-                    {name: 'Категория', field: 'name'}
-                ],
-                onRegisterApi: function (gridApi) {
-                    gridApi.selection.on.rowSelectionChanged($scope, filterItemsByCategory)
-                },
-                data: []
-            };
-
-            $http.get("data/categories.json").then(function (response) {
-                $scope.categoryGridOptions.data = response.data
-            });
-        }
-
-        function prepareCartGrid() {
-            $scope.cartGridOptions = {
-                gridMenuShowHideColumns: false,
-                enableColumnMenus: false,
-                enableGridMenu: true,
-                enableFiltering: true,
-                enableRowSelection: true,
-                enableRowHeaderSelection: false,
-                multiSelect: false,
-                enableCellEditOnFocus: true,
-                rowHeight: 25,
-                columnDefs: [
-                    {name: 'Код', field: 'code', width: '10%', enableCellEdit: false},
-                    {name: 'Артикул', field: 'article', width: '10%', enableCellEdit: false},
-                    {name: 'Наименование', field: 'name', width: '50%', enableCellEdit: false},
-                    {name: 'Количество', field: 'amount', width: '10%', enableFiltering: false, type: 'number'},
-                    {name: 'Цена', field: 'price', width: '10%', enableCellEdit: false, enableFiltering: false},
-                    {name: 'Сумма', field: 'sum', width: '10%', enableCellEdit: false, enableFiltering: false}
-                ],
-                onRegisterApi: function (gridApi) {
-                    $scope.cartGridApi = gridApi;
-                    gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
-                        if (rowEntity.amount) {
-                            rowEntity.sum = rowEntity.amount * rowEntity.price;
-                        }
-                    });
-                },
-                importerDataAddCallback: function (grid, newObjects) {
-                    $scope.cartGridOptions.data = $scope.cartGridOptions.data.concat(newObjects);
-                },
-                data: []
-            };
-
-            $scope.focusOnAmount = function (index) {
-                $timeout(function () {
-                    $scope.cartGridApi.cellNav.scrollToFocus(
-                        $scope.cartGridOptions.data[index],
-                        $scope.cartGridOptions.columnDefs[3]
-                    )
-                }, 100);
-            };
-        }
+      $http.get("data/items.json").then(function (response) {
+        $scope.itemsGridOptions.data = response.data;
+        allItems = response.data
+      });
     }
+
+    function focusOnItemRow() {
+      $timeout(function () {
+        var selected = $scope.itemsGridApi.cellNav.getCurrentSelection();
+        $scope.itemsGridApi.cellNav.scrollToFocus(
+          selected[0].row.entity,
+          $scope.itemsGridOptions.columnDefs[1]
+        )
+      }, 100)
+    }
+
+    function getChildrenCategories(row, ids) {
+      var children = row.treeNode.children;
+      if (children.length) {
+        children.forEach(function (child) {
+          getChildrenCategories(child.row, ids)
+        });
+      } else {
+        ids.push(row.entity.id);
+      }
+    }
+
+    function filterItemsByCategory(CategoriesRow) {
+      if (CategoriesRow.isSelected) {
+        var ids = [];
+        getChildrenCategories(CategoriesRow, ids);
+        $scope.itemsGridOptions.data = allItems.filter(function (item) {
+          return ids.indexOf(item.categoryId) >= 0
+        });
+      } else {
+        $scope.itemsGridOptions.data = allItems
+      }
+    }
+
+    function prepareCategoryGrid() {
+      $scope.categoryGridOptions = {
+        enableColumnMenus: false,
+        enableSorting: true,
+        enableFiltering: true,
+        enableColumnMenu: false,
+        showTreeExpandNoChildren: false,
+        enableRowSelection: true,
+        enableRowHeaderSelection: false,
+        multiSelect: false,
+        rowHeight: 25,
+        columnDefs: [
+          {name: 'Категория', field: 'name'}
+        ],
+        onRegisterApi: function (gridApi) {
+          gridApi.selection.on.rowSelectionChanged($scope, filterItemsByCategory)
+        },
+        data: []
+      };
+
+      $http.get("data/categories.json").then(function (response) {
+        $scope.categoryGridOptions.data = response.data
+      });
+    }
+
+    function prepareCartGrid() {
+      $scope.cartGridOptions = {
+        gridMenuShowHideColumns: false,
+        enableColumnMenus: false,
+        enableGridMenu: true,
+        enableFiltering: true,
+        enableRowSelection: true,
+        enableRowHeaderSelection: false,
+        multiSelect: false,
+        enableCellEditOnFocus: true,
+        rowHeight: 25,
+        columnDefs: [
+          {name: 'Код', field: 'code', width: '10%', enableCellEdit: false},
+          {name: 'Артикул', field: 'article', width: '10%', enableCellEdit: false},
+          {name: 'Наименование', field: 'name', width: '50%', enableCellEdit: false},
+          {name: 'Количество', field: 'amount', width: '10%', enableFiltering: false, type: 'number'},
+          {name: 'Цена', field: 'price', width: '10%', enableCellEdit: false, enableFiltering: false},
+          {name: 'Сумма', field: 'sum', width: '10%', enableCellEdit: false, enableFiltering: false}
+        ],
+        onRegisterApi: function (gridApi) {
+          $scope.cartGridApi = gridApi;
+          gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
+            if (rowEntity.amount) {
+              rowEntity.sum = rowEntity.amount * rowEntity.price;
+            }
+            focusOnItemRow();
+          });
+        },
+        importerDataAddCallback: function (grid, newObjects) {
+          $scope.cartGridOptions.data = $scope.cartGridOptions.data.concat(newObjects);
+        },
+        data: []
+      };
+
+      $scope.focusOnAmount = function (index) {
+        $timeout(function () {
+          $scope.cartGridApi.cellNav.scrollToFocus(
+            $scope.cartGridOptions.data[index],
+            $scope.cartGridOptions.columnDefs[3]
+          )
+        }, 100);
+      };
+    }
+
+    $scope.createOrder = function createOrder() {
+
+    }
+  }
 ]);
 
 
