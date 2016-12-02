6c394fa5ce796393e5aa5611d1f31c6b77ace134
Mikhail Gorbov <mgorbov@maprtech.com>
2016-12-02 13:57:18 +0200

    added delete 'button' to cart table

--- core/items/controller.js
+++ core/items/controller.js
@@ -48,6 +48,75 @@ angular.module('ordering').controller('ItemsCtrl', ['$scope', '$timeout', '$http
       };
     }
 
+    function prepareCartGrid() {
+      $scope.cartGridOptions = {
+        gridMenuShowHideColumns: false,
+        enableColumnMenus: false,
+        enableGridMenu: true,
+        enableFiltering: true,
+        enableRowSelection: true,
+        enableRowHeaderSelection: false,
+        multiSelect: false,
+        enableCellEditOnFocus: true,
+        rowHeight: 25,
+        columnDefs: [
+          {name: '', field:'del', width: '2%', enableFiltering: false, enableCellEdit: false, allowCellFocus : false,
+            cellTemplate: '<div href="" ng-click="grid.appScope.deleteRow(row)" class="text-center ui-grid-cell-contents">' +
+            '<a href=""><strong>Х</strong></a></div>'
+          },
+          {name: 'Код', field: 'code', width: '10%', enableCellEdit: false},
+          {name: 'Артикул', field: 'article', width: '10%', enableCellEdit: false},
+          {name: 'Наименование', field: 'name', width: '46%', enableCellEdit: false},
+          {name: 'Количество', field: 'amount', width: '10%', enableFiltering: false, type: 'number'},
+          {name: 'Цена', field: 'price', width: '10%', enableCellEdit: false, enableFiltering: false},
+          {name: 'Сумма', field: 'sum', width: '10%', enableCellEdit: false, enableFiltering: false}
+        ],
+        onRegisterApi: function (gridApi) {
+          $scope.cartGridApi = gridApi;
+          gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
+            calcSum(rowEntity);
+            calcTotalSum();
+          });
+        },
+        importerDataAddCallback: function (grid, newObjects) {
+          $scope.cartGridOptions.data = $scope.cartGridOptions.data.concat(newObjects);
+        },
+        data: $scope.$parent.cartGridData
+      };
+    }
+
+    function prepareCategoryGrid() {
+      $scope.categoryGridOptions = {
+        enableColumnMenus: false,
+        enableSorting: true,
+        enableFiltering: true,
+        enableColumnMenu: false,
+        showTreeExpandNoChildren: false,
+        enableRowSelection: true,
+        enableRowHeaderSelection: false,
+        multiSelect: false,
+        rowHeight: 25,
+        columnDefs: [
+          {name: 'Категория', field: 'name'}
+        ],
+        onRegisterApi: function (gridApi) {
+          gridApi.selection.on.rowSelectionChanged($scope, onCategoryChange)
+        },
+        data: []
+      };
+
+      $http.get("data/categories.json").then(function (response) {
+        $scope.categoryGridOptions.data = response.data
+      });
+    }
+
+    $scope.deleteRow = function(row) {
+      var index = $scope.cartGridOptions.data.indexOf(row.entity);
+      row.entity.amount = "";
+      $scope.cartGridOptions.data.splice(index, 1);
+      calcTotalSum();
+    };
+
     $scope.categoryFilter = function (renderedRows) {
       if ($scope.categoryIds.length > 0)
         renderedRows.forEach(function (row) {
@@ -77,31 +146,6 @@ angular.module('ordering').controller('ItemsCtrl', ['$scope', '$timeout', '$http
       $scope.itemsGridApi.grid.refresh();
     }
 
-    function prepareCategoryGrid() {
-      $scope.categoryGridOptions = {
-        enableColumnMenus: false,
-        enableSorting: true,
-        enableFiltering: true,
-        enableColumnMenu: false,
-        showTreeExpandNoChildren: false,
-        enableRowSelection: true,
-        enableRowHeaderSelection: false,
-        multiSelect: false,
-        rowHeight: 25,
-        columnDefs: [
-          {name: 'Категория', field: 'name'}
-        ],
-        onRegisterApi: function (gridApi) {
-          gridApi.selection.on.rowSelectionChanged($scope, onCategoryChange)
-        },
-        data: []
-      };
-
-      $http.get("data/categories.json").then(function (response) {
-        $scope.categoryGridOptions.data = response.data
-      });
-    }
-
     function calcSum(row) {
       if (row.amount) {
         row.sum = row.amount * row.price;
@@ -112,39 +156,6 @@ angular.module('ordering').controller('ItemsCtrl', ['$scope', '$timeout', '$http
       $scope.$parent.totalSum =  $scope.cartGridOptions.data.reduce(function (sum, row) {
         return sum + row.sum;
       }, 0)
-    }
-
-    function prepareCartGrid() {
-      $scope.cartGridOptions = {
-        gridMenuShowHideColumns: false,
-        enableColumnMenus: false,
-        enableGridMenu: true,
-        enableFiltering: true,
-        enableRowSelection: true,
-        enableRowHeaderSelection: false,
-        multiSelect: false,
-        enableCellEditOnFocus: true,
-        rowHeight: 25,
-        columnDefs: [
-          {name: 'Код', field: 'code', width: '10%', enableCellEdit: false},
-          {name: 'Артикул', field: 'article', width: '10%', enableCellEdit: false},
-          {name: 'Наименование', field: 'name', width: '50%', enableCellEdit: false},
-          {name: 'Количество', field: 'amount', width: '10%', enableFiltering: false, type: 'number'},
-          {name: 'Цена', field: 'price', width: '10%', enableCellEdit: false, enableFiltering: false},
-          {name: 'Сумма', field: 'sum', width: '10%', enableCellEdit: false, enableFiltering: false}
-        ],
-        onRegisterApi: function (gridApi) {
-          $scope.cartGridApi = gridApi;
-          gridApi.edit.on.afterCellEdit($scope, function (rowEntity, colDef, newValue, oldValue) {
-            calcSum(rowEntity);
-            calcTotalSum();
-          });
-        },
-        importerDataAddCallback: function (grid, newObjects) {
-          $scope.cartGridOptions.data = $scope.cartGridOptions.data.concat(newObjects);
-        },
-        data: $scope.$parent.cartGridData
-      };
     }
 
     $scope.createOrder = function createOrder() {
